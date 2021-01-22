1d26f672d10dc3c0b1f142fe61d0f064bd3c2979
Muhammad Arief <47475051+Sorong-stream@users.noreply.github.com>
2021-01-22 22:42:48 +0900

    Create kok.js

---
+++ kok.js
@@ -0,0 +1,110 @@
+/**
+
+
+
+
+
+
+
+
+
+
+SO INI GW PUNYA BABI GW M.A 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+MALING SCRIPT GAK BISA BUAT SENDIRI YA DEK :v
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */ $(document).ready(function() {
+        $('.canvas').particles({
+          connectParticles: true,
+          color: '#FF0B0B',
+          size: 2,
+          maxParticles: 120,
+          speed: 0.3
+        });
+/**
+
+
+
+
+
+
+
+
+
+
+SO INI GW PUNYA BABI GW M.A 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+MALING SCRIPT GAK BISA BUAT SENDIRI YA DEK :v
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */ 
+      });
